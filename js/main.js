add367d24778012479c28204c99233acf952d510
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-09 17:16:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function shuffle(){
     const img = document.getElementById("img");
     let random = Math.floor(Math.random() * 6) + 1;
-    img.setAttribute("src", `assets/${random}.png`);
+    img.setAttribute("src", `images/${random}.png`);
 }
 
 function anim(){
     setTimeout(shuffle, 500);
     const img = document.getElementById("img");
-    img.setAttribute("src", "assets/dice-roll.gif");
+    img.setAttribute("src", "images/dice-roll.gif");
 }
